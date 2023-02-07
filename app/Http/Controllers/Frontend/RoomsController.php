e25132aa8812bb1aefc976b884a9ee3da40df7ad
Anuruddha <e2041032@bit.mrt.ac.lk>
2023-02-07 23:31:21 +0530

    room listing

--- app/Http/Controllers/Frontend/RoomsController.php
+++ app/Http/Controllers/Frontend/RoomsController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\PropertyRooms;
 use Illuminate\Http\Request;
 
 class RoomsController extends FrontendController
 {
     public function rooms(){
 
-        return view('frontend.rooms');
+        $rooms = PropertyRooms::where('status', 1)->orderBy('created_at', 'DESC')->get();
+
+        return view('frontend.rooms', ['rooms' => $rooms]);
     }
 
     public function room($id){
